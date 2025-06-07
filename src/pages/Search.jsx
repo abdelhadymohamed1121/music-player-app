fc1b394f8cce2d649f858916536cd8959e5de606
abdelhadymohamed1121 <abdelhadymohamed1121@gmail.com>
2025-06-07 20:26:46 +0300

    final project

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
-import SearchCard from '../components/searchCard';
+import SearchCard from '../components/SearchCard';
 import Loader from '../components/Loader';
 import Error from '../components/Error';
 import { useGetSongsBySearch } from '../hooks/hooks';
