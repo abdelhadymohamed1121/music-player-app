ebf6ec827e6b6d9bd350b904433097ef268e5eb9
abdelhadymohamed1121 <abdelhadymohamed1121@gmail.com>
2025-06-07 20:37:35 +0300

    final project

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import SideBar from '../components/SideBar';
+import Sidebar from '../components/Sidebar.jsx';
 import Searchbar from '../components/Searchbar';
 import TopPlay from '../components/TopPlay';
 import MusicPlayer from '../components/MusicPlayer/index.jsx';
@@ -9,7 +9,7 @@ const Layout = () => {
   const { activeSong } = useSelector((state) => state.player);
   return (
     <div className="relative flex">
-      <SideBar />
+      <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
         <Searchbar />
 
